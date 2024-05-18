e52ea67ae04dfc344d62be98bfc9ffef1a7ab126
kennychou <u11002120@o365.ttu.edu.tw>
2024-05-19 00:02:46 +0800

    first commit新增了密碼

---
+++ E3A20-HW3.c
@@ -0,0 +1,39 @@
+#include<stdio.h>                                     /*把stdio.h這個檔案含括進來*/
+#include<stdlib.h>                                    /*把stdlib.h這個檔案含括進來*/
+#include<string.h>                                    /*把string.h這個檔案含括進來*/
+#include<conio.h>                                     /*載入conio.h標頭檔*/ 
+#include<time.h>                                      /*取得時間與日期、對時間與日期資料操作及格式化的標頭檔*/
+int main(void)                                        /*主函數main()從這開始*/
+{
+	printf("程式設計作業3\n");                        /*作業3 part 1:個人風格的封面*/
+	printf("\n");
+	printf("┴┬┴┬／￣＼＿／￣＼\n");
+	printf("┬┴┬┴▏　　▏▔▔▔▔＼\n");
+	printf("┴┬┴／＼　／　　　　　　﹨\n");
+	printf("┬┴∕　　　　　　　／　　　）\n");
+	printf("┴┬▏　　　　　　　　●　　▏\n");
+	printf("┬┴▏　　　　　　　　　　　▔█\n"); 
+	printf("┴◢██◣　　　　　　 ＼＿＿／\n");
+	printf("┬█████◣　　　　　　　／\n");
+	printf("┴█████████████◣\n");
+	printf("◢██████████████▆▄\n");
+	printf("◢██████████████▆▄\n");
+	printf("█◤◢██◣◥█████████◤＼\n");
+	printf("◥◢████　████████◤　　 ＼\n");
+	printf("┴█████　██████◤　　　　　 ﹨\n");
+	printf("┬│　　　│█████◤　　　　　　　　▏\n");
+	printf("┴│　　　│　　　　　　　　　　　　　　▏\n");
+	printf("┬∕　　　∕　　　　／▔▔▔＼　　　　 ∕\n");
+	printf("*∕＿＿_／﹨　　　∕　　　　　 ＼　　／＼\n");
+	printf("┬┴┬┴┬┴＼ 　　 ＼_　　　　　﹨／　　﹨\n");
+	printf("┴┬┴┬┴┬┴ ＼＿＿＿＼　　　　 ﹨／▔＼﹨／▔＼\n");
+	printf("╥╥╥╥╥╥╥╥╥╥╥╥╥╥＼　　 ∕　 ／▔﹨　\n");
+	system("pause");                                /*螢幕畫面暫停，並等待使用者按任意鍵 */
+	system("cls");                                  /*清除螢幕*/
+	
+	char ps[5]="2024";                                  
+	char en[5];                                   
+	int n=1,r,p,m=0,i,j;
+	char e,t,c;
+	char sw;
+
