2f3204edb1413f9f838ea3a4d3f611b0d6b01305
kennychou <u11002120@o365.ttu.edu.tw>
2024-05-19 00:10:33 +0800

    second commit 新增了第二題主選單

--- E3A20-HW3.c
+++ E3A20-HW3.c
@@ -36,4 +36,34 @@ int main(void)                                        /*主函數main()從這開始*/
 	int n=1,r,p,m=0,i,j;
 	char e,t,c;
 	char sw;
+do{                                                 /*要求使用者輸入4個數字的密碼(密碼值預設為2024)*/
+		printf("請輸入密碼:");
+		scanf("%s",en);
+		if(strcmp(ps,en)==0){                       /*若密碼等於預設值*/ 
+		break;
+		}		
+			else if(n==3){                          /*若連續錯三次，則顯示警告訊息並結束程式*/
+			printf("▲ 錯%d次!請重新登入\n",n);
+			printf("\a");
+				return 0;
+				}
+			else{			
+			printf("▲ 錯%d次!\n",n);
+			n++;
+			}
+				
+	}while(n<=3);	 
+
+			system("CLS");                          /*作業3 part 2:清除螢幕，螢幕出現主選單*/
+			menu:                              
+			puts("* * * * *[選 位 系 統]* * * * *");  
+			puts("*       a. 顯示現有座位       *");
+			puts("*       b. 電腦排位           *");
+			puts("*       c. 自主選位           *");
+			puts("*       d. 選位結束           *");
+			puts("* * * * * * * * * * * * * * * *");
+			printf("請輸入一個選項:");              /*要求使用者輸入一個字元(如主選單所示)*/
+			fflush(stdin);
+			scanf("%c",&sw);
+			                                        /*作業3 part 3:在9*9的預設座位中產生10個被預訂的亂數座位*/
 
